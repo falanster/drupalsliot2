9348b0a8a8d3e8644179f046ee797508340ef700
Pirate <pir@server>
2012-11-01 16:47:06 +0000

    added newer database

--- backups/dump.sh
+++ backups/dump.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-drush sql-dump --gzip > ./SLIOT-BACKUP-`date '+%m-%d-%H-%M'`.sql.gz
+mysqldump -ufalanste_drupal -p falanste_drupalclub > ./SLIOT-BACKUP-`date '+%m-%d-%H-%M'`.sql
 
+gzip SLIOT-BACKUP-`date '+%m-%d-%H'`*.sql
